222bfa2430762a811c1726d891cdffa35a4b18bc
agirltran2k1 <agirltran2k1@gmail.com>
2024-03-08 15:21:20 +0700

    aaa

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -85,7 +85,7 @@ export default function Footer() {
         id="footer-2"
         className="bg-blue_color_2 w-full flex text-white_color p-3 justify-center"
       >
-        <p>© 2023 ISGroup</p>
+        <p>© 2023 ISGroup 1</p>
       </div>
     </section>
   );
